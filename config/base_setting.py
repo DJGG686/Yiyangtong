579bb6d863cf9f63a50a7ea4429c62e9ba17705e
digjie <1243426834@qq.com>
2025-10-27 23:31:38 +0800

    设置修改

--- config/base_setting.py
+++ config/base_setting.py
@@ -4,3 +4,29 @@
 # @filename: base_setting
 # @function: 
 # @version : V1
+import os
+from datetime import timedelta
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Yiyangtong-Backend'
+    # 数据库配置
+    SQLALCHEMY_DATABASE_SETTING = {
+        'user': 'root',
+        'password': 'dj124342',
+        'host': 'localhost',
+        'database': 'college'
+    }
+
+    # JWT配置
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'Yiyangtong'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+
+    # 微信小程序配置
+    WECHAT_APPID = os.environ.get('WECHAT_APPID') or 'wxb264754c2abacc99'
+    WECHAT_SECRET = os.environ.get('WECHAT_SECRET') or '2e6209d0139be8b5a4902912f2ce7025'
+
+    # 日志配置
+    LOGGING_PATH = './logs'
+    LOGGING_CONFIG_PATH = './yaml_files/logging.yaml'
