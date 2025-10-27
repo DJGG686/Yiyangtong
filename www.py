86613d1e399a9d45718ba60b2b3d3f2347fdd270
digjie <1243426834@qq.com>
2025-10-27 23:31:28 +0800

    修改注册蓝图逻辑

--- www.py
+++ www.py
@@ -7,13 +7,18 @@
 # @description : 注册蓝图，管理路由
 
 from app import app
-from controller.institution_controller import InstitutionController
-from controller.schedule_controller import ScheduleController
-from controller.user_controller import UserController
+from controller import InstitutionController
+from controller import ScheduleController
+from controller import UserController
 
-app.register_blueprint(UserController().blueprint, url_prefix=f'/{UserController.PREFIX}')
-app.register_blueprint(InstitutionController().blueprint, url_prefix=f'/{InstitutionController.PREFIX}')
-app.register_blueprint(ScheduleController().blueprint, url_prefix=f'/{ScheduleController.PREFIX}')
+controllers = [
+    UserController(),
+    InstitutionController(),
+    ScheduleController(),
+]
+
+for controller in controllers:
+    controller.register(app)
 
 
 
