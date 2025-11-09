f7bb28164d74c287109ee4e33dc5e4205afb7da7
digjie <1243426834@qq.com>
2025-11-09 20:03:41 +0800

    微信api fix

--- utils/wechat_api.py
+++ utils/wechat_api.py
@@ -8,8 +8,7 @@
 import requests
 import jwt
 from flask import current_app
-from models import db, User, UserSession
-from datetime import datetime, timedelta
+from datetime import datetime
 
 
 class WeChatAPI:
@@ -19,20 +18,16 @@ class WeChatAPI:
         微信登录凭证校验
         文档: https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/user-login/code2Session.html
         """
-        appid = current_app.config['WECHAT_APPID']
-        secret = current_app.config['WECHAT_SECRET']
-
         url = 'https://api.weixin.qq.com/sns/jscode2session'
         params = {
-            'appid': appid,
-            'secret': secret,
+            'appid': current_app.config['WECHAT_APPID'],
+            'secret': current_app.config['WECHAT_SECRET'],
             'js_code': code,
             'grant_type': 'authorization_code'
         }
 
         try:
-            response = requests.get(url, params=params, timeout=10)
-            data = response.json()
+            data = requests.get(url, params=params, timeout=10).json()
 
             if 'errcode' in data and data['errcode'] != 0:
                 current_app.logger.error(f"WeChat API error: {data}")
@@ -140,47 +135,4 @@ class JWTUtils:
             current_app.logger.error("Invalid token")
             return None
 
-
-def create_or_update_user(wechat_data, user_info=None):
-    """
-    创建或更新用户信息
-    """
-    openid = wechat_data.get('openid')
-    session_key = wechat_data.get('session_key')
-
-    # 查找用户
-    user = User.query.filter_by(openid=openid).first()
-
-    if not user:
-        # 创建新用户
-        user = User(
-            openid=openid,
-            unionid=wechat_data.get('unionid')
-        )
-        db.session.add(user)
-        db.session.flush()  # 获取user id
-
-    # 更新用户信息（如果提供了用户信息）
-    if user_info:
-        user.nickname = user_info.get('nickName')
-        user.avatar_url = user_info.get('avatarUrl')
-        user.gender = user_info.get('gender', 0)
-        user.country = user_info.get('country')
-        user.province = user_info.get('province')
-        user.city = user_info.get('city')
-        user.language = user_info.get('language')
-
-    user.last_login = datetime.utcnow()
-
-    # 保存session
-    session = UserSession(
-        user_id=user.id,
-        session_key=session_key,
-        expires_at=datetime.utcnow() + timedelta(days=30)
-    )
-    db.session.add(session)
-
-    db.session.commit()
-
-    return user
 
